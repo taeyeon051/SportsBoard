dee00752a9d88b490e2e635aee84af120a4c7547
taeyeon051 <y2010112@y-y.hs.kr>
2021-11-08 20:50:07 +0900

    sql 수정

--- WebContent/table.sql
+++ WebContent/table.sql
@@ -3,16 +3,14 @@ drop table users;
 drop table users_seq;
 
 create table users (
-	id number(10),
 	userId varchar2(100),
 	userName varchar2(50),
-	userPwd varchar2(200)
+	userPwd varchar2(200),
+	constraint userId_pk primary key (userId)
 );
 
-CREATE SEQUENCE users_seq START WITH 1 INCREMENT BY 1 MAXVALUE 1000000 CYCLE NOCACHE;
-
 -- user table (register) --
-insert into users values(users_seq.nextval, 'admin', '관리자', '1234');
+insert into users values('admin', '관리자', '1234');
 select * from users;
 
 -- user table (login) --
