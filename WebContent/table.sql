dee97497989e198734e643d99c0674fee6657be1
taeyeon051 <y2010112@y-y.hs.kr>
2021-11-09 23:05:24 +0900

    sql 수정

--- WebContent/table.sql
+++ WebContent/table.sql
@@ -1,12 +1,11 @@
--- user table -- 
+/* user table */ 
 drop table users;
-drop table users_seq;
 
 create table users (
-	userId varchar2(100),
+	userId varchar2(100) not null,
 	userName varchar2(50),
 	userPwd varchar2(200),
-	constraint userId_pk primary key (userId)
+	constraint users_pk primary key (userId)
 );
 
 -- user table (register) --
@@ -15,3 +14,37 @@ select * from users;
 
 -- user table (login) --
 select * from users where userId = ? and userPwd = ?
+
+
+/* team table */
+drop table sports;
+drop table teams;
+
+create table sports (
+	s_code number(2) not null,
+	ko_sports_name varchar2(20),
+	en_sports_name varchar2(20),
+	constraint sports_pk primary key (s_code)
+);
+
+create table teams (
+	s_code number(2) not null,
+	team varchar2(50)
+);
+
+-- sports insert --
+insert into sports values (1, '야구', 'baseball');
+insert into sports values (2, '해외야구', 'worldbaseball');
+insert into sports values (3, '축구', 'soccer');
+insert into sports values (4, '해외축구', 'worldsoccer');
+insert into sports values (5, '골프', 'golf');
+insert into sports values (6, '농구', 'basketball');
+insert into sports values (7, '배구', 'volleyball');
+
+select * from sports;
+select * from teams;
+
+
+
+
+
