cc919cf1cbd8160e25a36afba521ce9c5cdc03c2
taeyeon051 <y2010112@y-y.hs.kr>
2021-11-12 23:30:07 +0900

    팀리스트 클릭이벤트 추가

--- WebContent/js/write.js
+++ WebContent/js/write.js
@@ -9,12 +9,32 @@ class App {
         this.teamList = [];
         this.teamDomList = [];
         this.playerList = [];
-        this.playerDomList = [];
+        this.playerDom = [];
 
         this.init();
     }
 
     init() {
+        this.teamDomList = document.querySelectorAll(".team-list>.btn");
+        this.playerDom = document.querySelectorAll(".player-list");
 
+        this.addEvent();
+    }
+
+    addEvent() {
+        const { teamDomList } = this;
+
+        $(teamDomList).on("click", e => {
+            const team = e.target;
+            if ($(team).hasClass("active")) {
+                team.classList.remove("active");
+                this.teamList.splice(this.teamList.indexOf(team.innerText), 1);
+            } else {
+                team.classList.add("active");
+                this.teamList.push(team.innerText);
+            }
+
+            log(this.teamList);
+        });
     }
 }
