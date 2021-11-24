3777eb0861d7dbc8033488b2b6beb1ba37bb54a3
taeyeon051 <y2010112@y-y.hs.kr>
2021-11-24 22:07:02 +0900

    sql 수정

--- WebContent/sql/write.sql
+++ WebContent/sql/write.sql
@@ -1,10 +1,10 @@
 /* 글 목록 테이블 */
 drop table writings;
 
-d writings;
 create table writings (
 	w_code number(5) not null,
 	w_type varchar2(50),
+	w_date varchar2(20),
 	title varchar2(200),
 	content clob,
 	teamList varchar2(500),
@@ -12,9 +12,8 @@ create table writings (
 	constraint writings_pk primary key (w_code)
 );
 
-select w_code, w_type, title, DBMS_LOB.SUBSTR(content, DBMS_LOB.GETLENGTH(content)), teamList, playerList from writings order by w_code;
+select w_code, w_type, title, DBMS_LOB.SUBSTR(content, DBMS_LOB.GETLENGTH(content)), teamList, playerList from writings where w_code = ? order by w_code;
 
 -- 글 번호 가져오기 --
 select max(w_code) + 1 as code from writings;
 -- 글 작성 --
-insert into writings w_code, w_type, title,  values(?, ?, ?, ?, ?);
