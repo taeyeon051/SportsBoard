5641f203ac525269510097cc085b2a1a79ebf691
taeyeon051 <y2010112@y-y.hs.kr>
2021-11-28 17:19:52 +0900

    sql 수정

--- WebContent/sql/write.sql
+++ WebContent/sql/write.sql
@@ -6,13 +6,18 @@ create table writings (
 	w_type varchar2(50),
 	w_date varchar2(20),
 	title varchar2(200),
+	writer varchar2(100),
 	content clob,
 	teamList varchar2(500),
 	playerList varchar2(500),
-	constraint writings_pk primary key (w_code)
+	constraint writings_pk primary key (w_code),
+	constraint type_check check (w_type in ('baseball', 'worldbaseball', 'soccer', 'wolrdsoccer', 'golf', 'basketball', 'volleyball'))
 );
 
 select w_code, w_type, title, DBMS_LOB.SUBSTR(content, DBMS_LOB.GETLENGTH(content)), teamList, playerList from writings where w_code = ? order by w_code;
 
 -- 글 번호 가져오기 --
 select max(w_code) + 1 as code from writings;
+
+-- 글 목록 가져오기 --
+select * from writings order by w_code desc;
