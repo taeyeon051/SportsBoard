f071e33f29964d5d6573c370d1e3ca0fff28d189
taeyeon051 <y2010112@y-y.hs.kr>
2021-11-30 23:54:59 +0900

    글 삭제 추가

---
+++ WebContent/js/delete.js
@@ -0,0 +1,35 @@
+const log = console.log;
+
+class Delete {
+    constructor(no, writerId) {
+        this.no = no;
+        this.writerId = writerId;
+        this.urlParams = new URLSearchParams(window.location.search);
+        this.type = this.urlParams.get('type');
+
+        this.addEvent();
+    }
+
+    addEvent() {
+        const { no, type, writerId } = this;
+
+        // 글 삭제 버튼 이벤트
+        $("#delete-btn").on("click", e => {
+            if (!confirm('정말 삭제하시겠습니까?')) return;
+            log('삭제');
+            $.ajax({
+                url: '/SportsBoard/board/delete',
+                type: "POST",
+                data: { no, type, writerId },
+                success: e => {
+                    log(e);
+                    location.href = `/SportsBoard/board/list?type=${type}&p=1`;
+                },
+                error: (req, err) => {
+                    console.log(req.status, err);
+                    location.href = `/SportsBoard/board/view?type=${type}&id=${no}`;
+                }
+            });
+        });
+    }
+}
---
+++ src/controller/DeletePageController.java
@@ -0,0 +1,40 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.BoardDAO;
+import vo.UserVO;
+
+public class DeletePageController implements Controller {
+	@Override
+	public MyView process(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		BoardDAO dao = new BoardDAO();
+		
+		HttpSession session = request.getSession();
+		UserVO user = (UserVO) session.getAttribute("user");
+		String writerId = request.getParameter("writerId");
+
+		if (!writerId.equals(user.getUserId())) {
+			request.setAttribute("alert", "해당 글을 삭제 할 권한이 없습니다.");
+			return null;
+		}
+		
+		int code = Integer.parseInt(request.getParameter("no"));
+		
+		String url;
+		int n = dao.deleteBoard(code);
+		if (n > 0) url = "/index.jsp";
+		else {
+			request.setAttribute("alert", "DB 오류로 인하여 글 삭제에 실패하였습니다.");
+			return null;
+		}
+		
+		return new MyView(url);
+	}
+}
